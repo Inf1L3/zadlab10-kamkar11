c259063fe239cbf76b094abd0ed5609cdcde922d
Kamil Machański <>
2018-05-16 17:20:32 +0200

    zadania od 1 do 3

--- OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
@@ -1,12 +1,54 @@
 package pl.edu.ur.oopl10;
 
-/**
- *
- * @author mchla
- */
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        
+        // 2
+        /*
+        int[] tablica = {32, 24, 32345, 4};
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Ktory element tablicy wyświetlic?");
+        int index = input.nextInt();
+        input.close();
+        try 
+        {
+            System.out.println(tablica[index]);
+        } 
+        catch(ArrayIndexOutOfBoundsException e) 
+        {
+            System.err.println("Podales indeks wykraczajacy poza rozmiar tablicy!");
+        }
+        */
+        
+        // 3
+        dzielenie();
+        
+    }
+    
+    static void dzielenie()
+    {
+        Scanner input = new Scanner(System.in);
+        
+        try {
+            
+           System.out.println("Wpisz liczby:");
+           
+           int n = input.nextInt();
+           int m = input.nextInt();
+           
+           double w = n / m;
+           
+           System.out.println(w);
+        }
+        catch (ArithmeticException e) 
+        {
+            System.err.println("Blad dzielenia przez 0 !!!!! ");
+        }
         
     }
     
