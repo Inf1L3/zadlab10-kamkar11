7bb8e0f313c80959f5ce579bc86497a6e57ed1b6
Kamil Machański <>
2018-05-22 19:49:01 +0200

    zadania 4 i 5

--- OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Main.java
@@ -1,12 +1,12 @@
 package pl.edu.ur.oopl10;
 
+import java.util.Random;
 import java.util.Scanner;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         // 2
         /*
         int[] tablica = {32, 24, 32345, 4};
@@ -23,33 +23,56 @@ public class Main {
         {
             System.err.println("Podales indeks wykraczajacy poza rozmiar tablicy!");
         }
-        */
-        
+         */
         // 3
+        /*
         dzielenie();
+         */
+        // 4 
+        
+       
+        Random r = new Random();
+
+        int a = r.nextInt(21) + (-10);
+        //int b = r.nextInt(21) + (-10);
+        int b = 0;
+
+        int licznik = 0;
+        while (licznik < 3) {
+            try {
+                int wynik = a / b;
+                System.out.println(wynik);
+            } catch (ArithmeticException e) {
+                System.out.println("blad dzielenia przez zero");
+                licznik++;
+                System.out.println("wyjatek zostal wywolany " + licznik + " raz");
+
+            }
+        }
+       
+        
+        
+        
         
     }
-    
-    static void dzielenie()
-    {
+
+    static void dzielenie() {
         Scanner input = new Scanner(System.in);
-        
+
         try {
-            
-           System.out.println("Wpisz liczby:");
-           
-           int n = input.nextInt();
-           int m = input.nextInt();
-           
-           double w = n / m;
-           
-           System.out.println(w);
-        }
-        catch (ArithmeticException e) 
-        {
+
+            System.out.println("Wpisz liczby:");
+
+            int n = input.nextInt();
+            int m = input.nextInt();
+
+            double w = n / m;
+
+            System.out.println(w);
+        } catch (ArithmeticException e) {
             System.err.println("Blad dzielenia przez 0 !!!!! ");
         }
-        
+
     }
-    
+
 }
--- OOPL10/src/main/java/pl/edu/ur/oopl10/WprowadzZKonsoli.java
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/WprowadzZKonsoli.java
@@ -1,10 +1,17 @@
 package pl.edu.ur.oopl10;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class WprowadzZKonsoli {
 
-    static void wpiszInt() {
+    public void wpiszInt() {
         Scanner input = new Scanner(System.in);
         try {
             int a = input.nextInt();
@@ -13,7 +20,7 @@ public class WprowadzZKonsoli {
         }
     }
 
-    static void wpiszString() {
+    public void wpiszString() {
         Scanner input = new Scanner(System.in);
         try {
             String a = input.nextLine();
@@ -22,7 +29,7 @@ public class WprowadzZKonsoli {
         }
     }
 
-    static void wpiszDouble() {
+    public void wpiszDouble() {
         Scanner input = new Scanner(System.in);
         try {
             Double a = input.nextDouble();
@@ -31,7 +38,7 @@ public class WprowadzZKonsoli {
         }
     }
 
-    static void wpiszFloat() {
+    public void wpiszFloat() {
         Scanner input = new Scanner(System.in);
         try {
             Float a = input.nextFloat();
@@ -39,4 +46,153 @@ public class WprowadzZKonsoli {
             System.out.println("Nie jest to float");
         }
     }
+
+    public void wpiszlong() {
+        Scanner input = new Scanner(System.in);
+        try {
+            long a = input.nextLong();
+        } catch (Exception e) {
+            System.out.println("Nie jest to long");
+        }
+    }
+
+    public void wpiszshort() {
+        Scanner input = new Scanner(System.in);
+        try {
+            short a = input.nextShort();
+        } catch (Exception e) {
+            System.out.println("Nie jest to short");
+        }
+    }
+
+    public void wpiszchar() {
+        Scanner input = new Scanner(System.in);
+        try {
+            char a = input.next().charAt(0);
+        } catch (Exception e) {
+            System.out.println("Nie jest to char");
+        }
+    }
+
+    /*
+    // czytanie
+    FileReader fr = new FileReader("nazwa.rozszerzenie");
+        BufferedReader br = new BufferedReader(fr);
+        while (stringRead != null) {
+            stringRead = br.readLine();
+            //...  rzutowanie na odpowiedni typ i podstawienie pod
+            //...  zmienną
+        }
+        br.close();
+    }
+     */
+ /*
+    // zapis
+    FileWriter fwo = new FileWriter("nazwa.rozszerzenie", `false`/`true`);
+        BufferedWriter bwo = new BufferedWriter(fwo);
+        bwo.write(...);
+        bwo.newLine();
+
+        bwo.close();
+    
+     */
+    public void zapiszInt(int x) {
+
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszString(String x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(x);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszDouble(double x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszFloat(float x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszLong(long x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszShort(short x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void zapiszChar(char x) {
+        try {
+            FileWriter fw = new FileWriter("zapis.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(String.valueOf(x));
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("blad zapisu");
+        }
+    }
+
+    public void odczyt(String x)  {
+        try {
+            File fr = new File(x);
+            Scanner in = new Scanner(fr);
+
+            String tekst = in.nextLine();
+            System.out.println(tekst);
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("nie znaleziono pliku");
+        }
+
+    }
+
 }
